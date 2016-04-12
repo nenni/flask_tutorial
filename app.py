3c28c32f01f5ee9ab4851cc769038d43a70ec4f9
Nikolay Nenchev <nikolay.nenchev@gmail.com>
2016-04-12 18:42:06 +0100

    initial test with flask-security

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask import render_template, request, url_for, redirect
+from flask.ext.security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 
 app = Flask(__name__)
@@ -8,7 +9,32 @@ app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
+
 import models
+
+# Define models
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
 
 @app.route('/')
@@ -16,17 +42,30 @@ def index():
     return "Hello Flask"
 
 
+# # Create a user to test with
+# @app.before_first_request
+# def create_user():
+#     db.create_all()
+#     user_datastore.create_user(email='matt@nobien.net', password='password')
+#     db.session.commit()
+
+
+# Views
+@app.route('/')
+@login_required
+def home():
+    return render_template('index.html')
+
+
 @app.route('/add_user')
 def add_user():
-    users = models.User.query.all()
-    oneuser = models.User.query.filter_by(name="test2").first()
-    return render_template("add_user.html", users=users, user2=oneuser)
+    return render_template("add_user.html")
 
 
-@app.route('/profile/<user_name>')
-def profile(user_name):
-    user = models.User.query.filter_by(name=user_name).first()
-    return render_template("profile.html", user=user)
+# @app.route('/profile/<user_name>')
+# def profile(user_name):
+#     user = models.User.query.filter_by(name=user_name).first()
+#     return render_template("profile.html", user=user)
 
 
 @app.route('/post_user', methods=['POST'])
--- config.py
+++ config.py
@@ -4,3 +4,8 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+# flask security module
+SECRET_KEY = 'super-secret'
+SECURITY_REGISTERABLE = True
+SECURITY_PASSWORD_HASH = 'sha512_crypt'
+SECURITY_PASSWORD_SALT = 'saltstring'
